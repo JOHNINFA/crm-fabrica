011a110f615a48b46250bc1c085ecc507bd9f793
JOHNINFA <johningonzalez2021@gmail.com>
2026-01-29 18:20:27 -0500

    fix

--- frontend/src/components/Print/TicketPreviewModal.jsx
+++ frontend/src/components/Print/TicketPreviewModal.jsx
@@ -280,7 +280,7 @@ export default function TicketPreviewModal({
                         border-bottom: none;
                         padding: 4px 2px 2px 2px;
                         font-weight: 900;
-                        font-size: ${tamanioTabla + 1}px;
+                        font-size: ${tamanioTabla + 2}px;
                         color: #000;
                         text-shadow: 0 0 0.3px #000, 0 0 0.3px #000;
                         -webkit-text-stroke: 0.3px #000;
